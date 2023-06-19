ce722858fd514184c605779d14c31671ab341e7f
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-06-19 20:07:08 +0800

    [+] Add ErrorMessage NO_RECORD_FOUND_BY_ID

--- HR/HR.Common/Constants/ErrorMessage.cs
+++ HR/HR.Common/Constants/ErrorMessage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HR.Common.Constants
+﻿namespace HR.Common.Constants
 {
     public class ErrorMessage
     {
